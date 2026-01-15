30a3ad9abb437032cd12abad22daa4de5ee6dd36
송승희 <victoria9981@fasoo.com>
2026-01-15 15:29:38 +0900

    feat: designsystem 추가

---
+++ core/designsystem/src/main/theme/Font.kt
@@ -0,0 +1,13 @@
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontFamily
+
+val soYo = FontFamily(
+    Font(R.font.soyo_maple_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.soyo_maple_regular, FontWeight.Normal, FontStyle.Normal)
+)
+val spoqahanSansneo = FontFamily(
+    Font(R.font.spoqahansansneo_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.spoqahansansneo_regular, FontWeight.Normal, FontStyle.Normal)
+)
