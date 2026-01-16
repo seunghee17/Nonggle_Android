db258c5bac450b700d717dc14e99b1d23914fb4d
seunghee <victoria9981@naver.com>
2026-01-16 22:58:10 +0900

    feat: app module에 navigation item 실구현

--- app/src/main/java/com/example/nonggleresume/navigation/TopLevelNavItem.kt
+++ app/src/main/java/com/example/nonggleresume/navigation/TopLevelNavItem.kt
@@ -1,21 +1,36 @@
 package com.example.nonggleresume.navigation
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.outlined.Refresh
-import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.designsystem.icon.NonggleIcons
+import com.example.home.R as homeResource
+import com.example.download.R as downloadResource
+import com.example.setting.R as settingResource
 
 data class TopLevelNavItem(
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector,
+    @DrawableRes val selectedIconRes: Int,
+    @DrawableRes val unselectedIconRes: Int,
     @StringRes val iconTextId: Int,
     @StringRes val titleTextId: Int,
 )
 
 val HOME = TopLevelNavItem(
-    selectedIcon = Icons.Filled.Home,
-    unselectedIcon = Icons.Outlined.Refresh,
-    iconTextId = forYouR.string.feature_foryou_api_title,
-    titleTextId = R.string.app_name,
+    selectedIconRes = NonggleIcons.homeSelected,
+    unselectedIconRes = NonggleIcons.homeUnselected,
+    iconTextId = homeResource.string.feature_name,
+    titleTextId = homeResource.string.feature_name,
+)
+
+val DOWNLOAD = TopLevelNavItem(
+    selectedIconRes = NonggleIcons.downloadSelected,
+    unselectedIconRes = NonggleIcons.downloadUnselected,
+    iconTextId = downloadResource.string.feature_name,
+    titleTextId = downloadResource.string.feature_name,
+)
+
+val SETTING = TopLevelNavItem(
+    selectedIconRes = NonggleIcons.homeSelected,
+    unselectedIconRes = NonggleIcons.homeUnselected,
+    iconTextId = settingResource.string.feature_name,
+    titleTextId = settingResource.string.feature_name,
 )
