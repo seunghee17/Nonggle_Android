3dd5e0a54d84d5c3b5640af18b2f9ead458b7c8c
seunghee <victoria9981@naver.com>
2026-01-17 15:47:14 +0900

    feat: initial commit

--- app/src/main/java/com/example/nonggleresume/ui/theme/Color.kt
+++ app/src/main/java/com/example/nonggleresume/ui/theme/Color.kt
@@ -1,7 +1,7 @@
 package com.example.nonggleresume.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
+// 컬러코드 정의 예정
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
